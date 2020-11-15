041f9c3903e0428b1f6c0a9389216295f31f4463
vlad <shapeofvoice2001@gmail.com>
2020-11-15 14:39:17 +0300

    labs

---
+++ Third course/MMIPiU/lab_02/lab_02/lab_02.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+class model
+{
+public:
+    virtual float equation(float yt, float ut) = 0;
+};
+
+class linear : public model
+{
+private:
+    float a, b, yt1;
+public:
+    linear(float a, float b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    float equation(float yt, float ut)
+    {
+        yt1 = a * yt + b * ut;
+        return yt1;
+    }
+};
+
+class nonlinear : public model
+{
+private:
+    float a, c, d, b;
+    float yt0 = 0, yt1;
+    float ut0 = 0;
+public:
+    nonlinear(float a, float b, float c, float d)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->d = d;
+    }
+
+    float equation(float yt, float ut)
+    {
+        yt1 = a * yt - b * pow(yt0, 2) + c * ut + d * sin(ut0);
+        ut0 = ut;
+        yt0 = yt;
+        yt = yt1;
+        return yt1;
+    }
+};
+
+class regulator
+{
+private:
+    float T, T0, TD, K, yt1 = 0;
+public:
+    regulator(float T, float T0, float TD, float K) {
+        this->T = T;
+        this->T0 = T0;
+        this->TD = TD;
+        this->K = K;
+    }
+
+    float warm(float e, float em1, float em2) {
+        float q0 = K * (1 + TD / T0);
+        float q1 = -K * (1 + 2 * TD / T0 - T0 / T);
+        float q2 = K * TD / T0;
+        yt1 += q0 * e + q1 * em1 + q2 * em2;
+        return yt1;
+    }
+};
+void PIDregulator(float w, float y0, regulator* reg, model* md) {
+    float e = 0, em1 = 0, em2 = 0, u = 0, y = y0;
+    for (int i = 0; i < 100; i++) {
+        e = w - y;
+        u = reg->warm(e, em1, em2);
+        y = md->equation(y0, u);
+        cout << "E=" << e << " Y=" << y << " U=" << u << endl;
+        em2 = em1;
+        em1 = e;
+    }
+}
+
+int main()
+{
+    cout << "Linear Model:" << endl;
+    linear* l = new linear(7, 0.27);
+    regulator* rl = new regulator(10, 10, 50, 0.1);
+    PIDregulator(3, 2, rl, l);
+    cout << endl;
+    cout << "NonLinear Model:" << endl;
+    nonlinear* nl = new nonlinear(7, 0.27, 0.2, 4);
+    regulator* rnl = new regulator(10, 10, 50, 0.1);
+    PIDregulator(3, 2, rnl, nl);
+    return 0;
+}
---
+++ Third course/OOPiP/lab_03/lab_03/lab_03.cpp
@@ -0,0 +1,319 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Armchair {
+public:
+	virtual void info() = 0;
+	virtual ~Armchair() {}
+};
+
+class Sofa {
+public:
+	virtual void info() = 0;
+	virtual ~Sofa() {}
+};
+
+class Cupboard {
+public:
+	virtual void info() = 0;
+	virtual ~Cupboard() {}
+};
+
+class Table {
+public:
+	virtual void info() = 0;
+	virtual ~Table() {}
+};
+
+class ArtDecoArmchair : public Armchair {
+public:
+	void info() {
+		cout << "ArtDecoArmchair" << endl;
+	}
+};
+
+class ArtDecoSofa : public Sofa {
+public:
+	void info() {
+		cout << "ArtDecoSofa" << endl;
+	}
+};
+
+class ArtDecoCupboard : public Cupboard {
+public:
+	void info() {
+		cout << "ArtDecoCupboard" << endl;
+	}
+};
+
+class ArtDecoTable : public Table {
+public:
+	void info() {
+		cout << "ArtDecoTable" << endl;
+	}
+};
+
+class VictorianArmchair : public Armchair {
+public:
+	void info() {
+		cout << "VictorianArmchair" << endl;
+	}
+};
+
+class VictorianSofa : public Sofa {
+public:
+	void info() {
+		cout << "VictorianSofa" << endl;
+	}
+};
+
+class VictorianCupboard : public Cupboard {
+public:
+	void info() {
+		cout << "VictorianCupboard" << endl;
+	}
+};
+
+class VictorianTable : public Table {
+public:
+	void info() {
+		cout << "VictorianTable" << endl;
+	}
+};
+
+class ModernArmchair : public Armchair {
+public:
+	void info() {
+		cout << "ModernArmchair" << endl;
+	}
+};
+
+class ModernSofa : public Sofa {
+public:
+	void info() {
+		cout << "ModernSofa" << endl;
+	}
+};
+
+class ModernCupboard : public Cupboard {
+public:
+	void info() {
+		cout << "ModernCupboard" << endl;
+	}
+};
+
+class ModernTable : public Table {
+public:
+	void info() {
+		cout << "ModernTable" << endl;
+	}
+};
+
+class PopArtArmchair : public Armchair {
+public:
+	void info() {
+		cout << "PopArtArmchair" << endl;
+	}
+};
+
+class PopArtSofa : public Sofa {
+public:
+	void info() {
+		cout << "PopArtSofa" << endl;
+	}
+};
+
+class PopArtCupboard : public Cupboard {
+public:
+	void info() {
+		cout << "PopArtCupboard" << endl;
+	}
+};
+
+class PopArtTable : public Table {
+public:
+	void info() {
+		cout << "PopArtTable" << endl;
+	}
+};
+
+class FurnitureFactory {
+public:
+	virtual Armchair* createArmchair() = 0;
+	virtual Sofa* createSofa() = 0;
+	virtual Cupboard* createCupboard() = 0;
+	virtual Table* createTable() = 0;
+	virtual ~FurnitureFactory() {}
+};
+
+class ArtDecoFurnitureFactory : public FurnitureFactory {
+public:
+	Armchair* createArmchair() {
+		return new ArtDecoArmchair;
+	}
+
+	Sofa* createSofa() {
+		return new ArtDecoSofa;
+	}
+
+	Cupboard* createCupboard() {
+		return new ArtDecoCupboard;
+	}
+
+	Table* createTable() {
+		return new ArtDecoTable;
+	}
+};
+
+class VictorianFurnitureFactory : public FurnitureFactory {
+public:
+	Armchair* createArmchair() {
+		return new VictorianArmchair;
+	}
+
+	Sofa* createSofa() {
+		return new VictorianSofa;
+	}
+
+	Cupboard* createCupboard() {
+		return new VictorianCupboard;
+	}
+
+	Table* createTable() {
+		return new VictorianTable;
+	}
+};
+
+class ModernFurnitureFactory : public FurnitureFactory {
+public:
+	Armchair* createArmchair() {
+		return new ModernArmchair;
+	}
+
+	Sofa* createSofa() {
+		return new ModernSofa;
+	}
+
+	Cupboard* createCupboard() {
+		return new ModernCupboard;
+	}
+
+	Table* createTable() {
+		return new ModernTable;
+	}
+};
+
+class PopArtFurnitureFactory : public FurnitureFactory {
+public:
+	Armchair* createArmchair() {
+		return new PopArtArmchair;
+	}
+
+	Sofa* createSofa() {
+		return new PopArtSofa;
+	}
+
+	Cupboard* createCupboard() {
+		return new PopArtCupboard;
+	}
+
+	Table* createTable() {
+		return new PopArtTable;
+	}
+};
+
+class Furniture {
+public:
+	~Furniture() {
+		int i;
+		for (i = 0; i < vecArmchair.size(); ++i) delete vecArmchair[i];
+		for (i = 0; i < vecSofa.size(); ++i) delete vecSofa[i];
+		for (i = 0; i < vecCupboard.size(); ++i) delete vecCupboard[i];
+		for (i = 0; i < vecTable.size(); ++i) delete vecTable[i];
+	}
+
+	void info() {
+		int i;
+		for (i = 0; i < vecArmchair.size(); ++i) vecArmchair[i]->info();
+		for (i = 0; i < vecSofa.size(); ++i) vecSofa[i]->info();
+		for (i = 0; i < vecCupboard.size(); ++i) vecCupboard[i]->info();
+		for (i = 0; i < vecTable.size(); ++i) vecTable[i]->info();
+	}
+
+	vector<Armchair*> vecArmchair;
+	vector<Sofa*> vecSofa;
+	vector<Cupboard*> vecCupboard;
+	vector<Table*> vecTable;
+};
+
+class Shop {
+public:
+	Furniture* createFurniture(FurnitureFactory& factory) {
+		Furniture* f = new Furniture;
+		f->vecArmchair.push_back(factory.createArmchair());
+		f->vecSofa.push_back(factory.createSofa());
+		f->vecCupboard.push_back(factory.createCupboard());
+		f->vecTable.push_back(factory.createTable());
+		return f;
+	}
+};
+
+int main() {
+	int menuNum;
+	FurnitureFactory* ff;
+	bool skip = false;
+	Shop shop;
+	PopArtFurnitureFactory pa_factory;
+	VictorianFurnitureFactory v_factory;
+	ModernFurnitureFactory m_factory;
+	ArtDecoFurnitureFactory ad_factory;
+	
+	while (!skip) {
+		cout << "Furniture configurator 1.0.0." << endl << "1. PopArt" << endl << "2. Victorian" << endl << "3. Modern" << endl << "4. ArtDeco" << endl << "0. Exit" << endl;
+		cin >> menuNum;
+		
+		switch (menuNum) {
+		case 1:
+			ff = new PopArtFurnitureFactory;
+			break;
+		case 2:
+			ff = new VictorianFurnitureFactory;
+			break;
+		case 3:
+			ff = new ModernFurnitureFactory;
+			break;
+		case 4:
+			ff = new ArtDecoFurnitureFactory;
+			break;
+		default:
+			ff = new PopArtFurnitureFactory;
+			skip = true;
+			std::cout << "Exited";
+			break;
+		}
+		
+		if (!skip) {
+			cout << "Furniture configurator 1.0.0." << endl << "1. Armchair" << endl << "2. Sofa" << endl << "3. Cupboard" << endl << "4. Table" << endl;
+			cin >> menuNum;
+			
+			switch (menuNum) {
+			case 1:
+				ff->createArmchair()->info();
+				break;
+			case 2:
+				ff->createSofa()->info();
+				break;
+			case 3:
+				ff->createCupboard()->info();
+				break;
+			case 4:
+				ff->createTable()->info();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
